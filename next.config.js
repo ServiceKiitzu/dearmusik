7991bc4bc28d3398c11dbdd04799767760a2fbe3
Frank Liao <frank_liao@kiitzu.com>
2022-05-21 00:27:08 +0800

    feat: i18n & scss settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  i18n: {
+    locales: ["zh", "en"],
+    defaultLocale: "zh",
+  },
+};
