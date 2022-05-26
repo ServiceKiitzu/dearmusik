9557d75fb7444b07cc1d736343e5af793e39906d
Frank Liao <frank_liao@kiitzu.com>
2022-05-27 06:28:13 +0800

    fix: _document.js bug

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Cookie from "./Cookie";
-import { Head, NextScript, Html } from "next/document";
 
 const Layout = ({ children }) => {
   return (
