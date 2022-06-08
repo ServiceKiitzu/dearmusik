0d2bafe78cadf9c20a10472ec6bd07e793127224
Frank Liao <frank_liao@kiitzu.com>
2022-06-08 17:05:47 +0800

    feat: about page( mobile )
    ref: http://fox.designforcode.cc/dearmusik/pm_dearmusik/-/issues/2

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Avatar from "../assets/about/avatar.png";
+import AvatarM from "../assets/about/avatar-m.png";
 import Fish from "../assets/about/fish.svg";
 import Text from "../assets/about/text.svg";
+import FishM from "../assets/about/fish-m.svg";
+import TextM from "../assets/about/text-m.svg";
 import styles from "../styles/About.module.scss";
 import { useRouter } from "next/router";
 import zh from "../locales/about/zh";
@@ -13,14 +16,23 @@ const About = () => {
   const t = locale === "zh" ? zh : en;
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.title}>關於</div>
+      <div className={styles.avatar}>
         <Image src={Avatar}></Image>
+      </div>
+      <div className={styles.avatarM}>
+        <Image src={AvatarM}></Image>
       </div>
       <div
         className={styles.story}
         dangerouslySetInnerHTML={{ __html: t.text }}
       ></div>
-
+      <div className={styles.imgM}>
+        <Image src={FishM}></Image>
+      </div>
+      <div className={styles.imgTextM}>
+        <Image src={TextM}></Image>
+      </div>
       <div className={styles.img}>
         <Image src={Fish}></Image>
       </div>
