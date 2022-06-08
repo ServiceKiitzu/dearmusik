732737eec746a18902665d37496f5341693ea603
Frank Liao <frank_liao@kiitzu.com>
2022-06-08 16:55:35 +0800

    feat: index page carousel img update

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Cookie from "./Cookie";
+import Image from "next/image";
+import StoreM from "../assets/index/store-m.svg";
 
 const Layout = ({ children }) => {
   return (
@@ -9,6 +11,9 @@ const Layout = ({ children }) => {
       <div className="content">{children}</div>
       <Cookie></Cookie>
       <Footer></Footer>
+      <div className="storeM">
+        <Image src={StoreM}></Image>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,20 @@ import "swiper/css/effect-fade";
 import { Navigation, EffectFade, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import I1 from "../assets/index/swipe_1.png";
-import I2 from "../assets/index/test1.webp";
-import I3 from "../assets/index/test2.jpg";
+import I1 from "../assets/index/swipe-1.jpg";
+import I2 from "../assets/index/swipe-2.jpg";
+import I3 from "../assets/index/swipe-3.jpg";
+import I4 from "../assets/index/swipe-4.jpg";
+import I5 from "../assets/index/swipe-5.jpg";
+import M1 from "../assets/index/swipe-m-1.jpg";
+import M2 from "../assets/index/swipe-m-2.jpg";
+import M3 from "../assets/index/swipe-m-3.jpg";
+import M4 from "../assets/index/swipe-m-4.jpg";
+import M5 from "../assets/index/swipe-m-5.jpg";
+import StoreM from "../assets/index/store-m.svg";
 import LArrow from "../assets/index/left_arrow.svg";
 import RArrow from "../assets/index/right_arrow.svg";
+import { CarouselItem } from "react-bootstrap";
 
 export default function Home() {
   const router = useRouter();
@@ -24,9 +33,43 @@ export default function Home() {
     const locale = e.target.value;
     router.push("/", "/", { locale });
   };
-
+  const carousel = [I1, I2, I3, I4, I5];
+  const carouselM = [M1, M2, M3, M4, M5];
   return (
     <>
+      <div className={styles.mask}></div>
+      <div className={styles.containerM}>
+        <Swiper
+          modules={[Autoplay, Navigation, EffectFade]}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          effect={"fade"}
+          navigation={{
+            nextEl: ".next-img",
+            prevEl: ".prev-img",
+          }}
+        >
+          <div className={`${styles.p} prev-img`}>
+            <Image src={LArrow}></Image>
+          </div>
+          <div className={`${styles.n} next-img`}>
+            <Image src={RArrow}></Image>
+          </div>
+          {carouselM &&
+            carouselM.map((item, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <div>
+                    <Image src={item}></Image>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+      </div>
       <div className={styles.container}>
         <Swiper
           modules={[Autoplay, Navigation, EffectFade]}
@@ -47,21 +90,16 @@ export default function Home() {
           <div className={`${styles.n} next-img`}>
             <Image src={RArrow}></Image>
           </div>
-          <SwiperSlide>
-            <div>
-              <Image src={I1}></Image>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <Image src={I2}></Image>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <Image src={I3}></Image>
-            </div>
-          </SwiperSlide>
+          {carousel &&
+            carousel.map((item, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <div>
+                    <Image src={item}></Image>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
         </Swiper>
       </div>
     </>
