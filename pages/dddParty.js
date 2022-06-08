a55031d5abbdcd600093008bcb48fa254130486f
Frank Liao <frank_liao@kiitzu.com>
2022-06-08 17:06:20 +0800

    feat: party page( mobile )
    ref: http://fox.designforcode.cc/dearmusik/pm_dearmusik/-/issues/3

--- pages/dddParty.js
+++ pages/dddParty.js
@@ -1,12 +1,16 @@
 import styles from "../styles/Party.module.scss";
 import Image from "next/image";
 import Soon from "../assets/ddd-party/soon.svg";
+import SoonM from "../assets/ddd-party/soon-m.svg";
 
 const DDDParty = () => {
   return (
     <div className={styles.container}>
       <div className={styles.img}>
         <Image src={Soon}></Image>
+      </div>
+      <div className={styles.imgM}>
+        <Image src={SoonM}></Image>
       </div>
       <div className={styles.block}></div>
     </div>
