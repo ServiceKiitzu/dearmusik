5c790ed132ea3b4599209fd416f978ab7781fd37
Frank Liao <frank_liao@kiitzu.com>
2022-06-17 04:42:28 +0800

    feat: music page resource update
    ref: http://fox.designforcode.cc/dearmusik/pm_dearmusik/-/issues/4#note_9365

--- pages/works/music.js
+++ pages/works/music.js
@@ -22,6 +22,7 @@ import S8 from "../../assets/works/music/S8.png";
 import S9 from "../../assets/works/music/S9.png";
 import S10 from "../../assets/works/music/S10.png";
 import S11 from "../../assets/works/music/S11.png";
+import S12 from "../../assets/works/music/S12.png";
 import { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 
@@ -29,91 +30,214 @@ const Music = () => {
   const musics = [
     {
       id: 1,
-      year: "2007",
+      year: "2008",
       title1: "綠色的日子",
       title2: "",
       feat: "",
       sImg: S1,
+      link: [
+        "https://music.apple.com/tw/album/%E7%B6%A0%E8%89%B2%E7%9A%84%E6%97%A5%E5%AD%90-ep/893230062",
+        "https://open.spotify.com/album/06NGQHBfbrFOGTV18UtWvY?si=qjNc2QrQSDyL8WP1Zp3sBw",
+        "https://kkbox.fm/Ia020D",
+      ],
+      song: ["Dadadalada", "綠色的日子", "存在", "25歲", "做朋友", "香格里拉"],
     },
     {
       id: 2,
-      year: "2014",
-      title1: "放個假",
-      title2: "(SIn)",
-      feat: "",
-      sImg: S2,
-    },
-    {
-      id: 3,
       year: "2010",
       title1: "我的高中同學",
       title2: "",
       feat: "",
+      sImg: S2,
+      link: [
+        "https://music.apple.com/tw/album/%E6%88%91%E7%9A%84%E9%AB%98%E4%B8%AD%E5%90%8C%E5%AD%B8/1446584507",
+        "https://open.spotify.com/album/3H7cyAVtrjPEuJQss7SQdv?si=hz2-aurJTI-mJiobSGHyAQ",
+        "https://kkbox.fm/1a3pfd",
+      ],
+      song: [
+        "我的高中同學",
+        "今天星期幾",
+        "夢的距離",
+        "台北",
+        "Balaba",
+        "花蝴蝶",
+        "最害羞的情感",
+        "忘了",
+        "拾荒地圖",
+        "生活一堆毛",
+        "我和你",
+      ],
+    },
+    {
+      id: 3,
+      year: "2013",
+      title1: "下雨的晚上",
+      title2: "",
+      feat: "",
       sImg: S3,
+      link: [
+        "https://music.apple.com/tw/album/%E4%B8%8B%E9%9B%A8%E7%9A%84%E6%99%9A%E4%B8%8A/767147817",
+        "https://open.spotify.com/album/4zHBCvRy2ZJRmT25RCXAeS?si=YWlcj-HGSza6w-MyNdEz_w",
+        "https://kkbox.fm/ja0Xn0",
+      ],
+      song: [
+        "問題",
+        "Diana",
+        "下雨的晚上",
+        "慢動作",
+        "放個假",
+        "一起跳舞",
+        "我可以",
+        "跟你出去玩",
+        "如果連你都笑了",
+        "流行歌",
+        "全世界的人都很悲傷",
+      ],
     },
     {
       id: 4,
+      year: "2013",
+      title1: "放個假",
+      title2: "",
+      feat: "(黃玠x蛋堡)",
+      sImg: S4,
+      link: [
+        "https://music.apple.com/tw/album/%E6%94%BE%E5%80%8B%E5%81%87-single/767256272",
+        "https://open.spotify.com/album/3xeN769subv05HMxjAov3v?si=tMTakj6ASLalQCdZsQY8sw",
+        "https://kkbox.fm/ja0bih",
+      ],
+      song: ["放個假(vs. 蛋堡)", "少年維持著煩惱"],
+    },
+    {
+      id: 5,
       year: "2014",
       title1: "大自然的力量",
       title2: "",
       feat: "",
-      sImg: S4,
-    },
-    {
-      id: 5,
-      year: "2015",
-      title1: "十年熟成",
-      title2: "",
-      feat: "",
       sImg: S5,
+      link: [
+        "https://music.apple.com/tw/album/%E5%A4%A7%E8%87%AA%E7%84%B6%E7%9A%84%E5%8A%9B%E9%87%8F-feat-%E5%A5%B3%E5%AD%A9%E8%88%87%E6%A9%9F%E5%99%A8%E4%BA%BA-%E8%88%92%E7%B1%B3%E6%81%A9-%E9%BB%83%E5%B0%8F%E6%A5%A8-ep/941950340",
+        "https://open.spotify.com/album/1hqK5A4XwIoErBuVdZRraI?si=5IsATupFQvWtFG6_X5G40g",
+        "https://kkbox.fm/Oa0j1Z",
+      ],
+      song: [
+        "因為你 (vs. 女孩與機器人)",
+        "一路向東 (vs. Suming 舒米恩)",
+        "你是不是像我一樣 (vs. 黃小楨)",
+        "寂寞殺死我 (vs. 黃小楨)",
+        "旅途 (vs. Suming 舒米恩)",
+        "跟蹤 (vs. 女孩與機器人)",
+      ],
     },
     {
       id: 6,
+      year: "2015",
+      title1: "十年熟成精選輯",
+      title2: "",
+      feat: "(黃玠xNabowa)",
+      sImg: S6,
+      link: [
+        "https://music.apple.com/tw/album/%E5%8D%81%E5%B9%B4%E7%86%9F%E6%88%90%E7%B2%BE%E9%81%B8%E8%BC%AF-single/1054708532",
+        "https://open.spotify.com/album/16ylvovloECoZcalxPfDz6?si=pCh_UFL1Sfi5L2Xn4u9P1A",
+        "https://kkbox.fm/ia0tTLL",
+      ],
+      song: [
+        "她的生活 (Diana's Weekend) (feat. 黃玠)",
+        "Giant's Walking (Ramen Version) (feat. 黃玠)",
+      ],
+    },
+    {
+      id: 7,
       year: "2016",
       title1: "一片黑暗之中",
       title2: "",
       feat: "",
-      sImg: S6,
-    },
-    {
-      id: 7,
-      year: "2017",
-      title1: "我們",
-      title2: "(Sin)",
-      feat: "",
       sImg: S7,
+      link: [
+        "https://music.apple.com/tw/album/%E5%9C%A8%E4%B8%80%E7%89%87%E9%BB%91%E6%9A%97%E4%B9%8B%E4%B8%AD/1118852031",
+        "https://open.spotify.com/album/6YeIHLRFUzowPDBf7YTp03?si=CrihMeSUSSaekMWDdQpQEQ",
+        "https://kkbox.fm/5a17Kd",
+      ],
+      song: [
+        "在一片黑暗之中",
+        "關於那些記得的事",
+        "走吧",
+        "你",
+        "她的生活",
+        "當...",
+        "我很想念你",
+        "不必哀怨",
+        "Party 到天亮",
+        "脆弱",
+      ],
     },
     {
       id: 8,
       year: "2017",
-      title1: "海邊情歌",
-      title2: "(sin)",
+      title1: "我們",
+      title2: "",
       feat: "",
       sImg: S8,
+      link: [
+        "https://music.apple.com/tw/album/%E6%88%91%E5%80%91-single/1224781969",
+        "https://open.spotify.com/album/6xnNRHxqenbEuY7a4sDbfR?si=8R51uPlpSCOb2j9XHdIcjA",
+        "https://kkbox.fm/fa1uCR",
+      ],
+      song: ["我們"],
     },
     {
       id: 9,
       year: "2017",
-      title1: "相片蒐集簿",
-      title2: "(Sin)",
+      title1: "海邊情歌",
+      title2: "",
       feat: "",
       sImg: S9,
+      link: [
+        "https://music.apple.com/tw/album/%E6%B5%B7%E9%82%8A%E6%83%85%E6%AD%8C-single/1253861390",
+        "https://open.spotify.com/album/2mAIB8iEJyfDvPrxnSrCw8?si=xzUR_byPSYqOWq8eyXoOoA",
+        "https://kkbox.fm/pa1S4c",
+      ],
+      song: ["海邊情歌"],
     },
     {
       id: 10,
-      year: "2019",
-      title1: "安卓雅—",
-      title2: "像月亮的人",
-      feat: "feat.黃玠",
+      year: "2017",
+      title1: "相片蒐集簿",
+      title2: "",
+      feat: "",
       sImg: S10,
+      link: [
+        "https://music.apple.com/tw/album/%E7%9B%B8%E7%89%87%E8%92%90%E9%9B%86%E7%B0%BF-single/1317671947",
+        "https://open.spotify.com/album/2yq3To9A4mOGPljfffeOLy?si=EGnEsqclRG2ZxC4-99NTPw",
+        "https://kkbox.fm/3a2IsW",
+      ],
+      song: ["相片蒐集簿"],
     },
     {
       id: 11,
-      year: "2021",
-      title1: "PUZZLEMAN—",
-      title2: "打打多多",
-      feat: "feat.黃玠",
+      year: "2019",
+      title1: "激膚—",
+      title2: "像月亮的人",
+      feat: "(feat. 黃玠)",
       sImg: S11,
+      link: [
+        "https://music.apple.com/tw/album/%E5%83%8F%E6%9C%88%E4%BA%AE%E7%9A%84%E4%BA%BA/1464645329?i=1464645338",
+        "https://open.spotify.com/track/2XFyo3PL3JnR0cr5eVgKq6?si=9f801b45cb864042",
+        "https://kkbox.fm/KsUBLD?utm_source=share&utm_medium=song&utm_campaign=%E6%BF%80%E8%86%9A%E6%A8%82%E5%9C%98+%28My+Skin+Against+Your+Skin%29+-+%E5%83%8F%E6%9C%88%E4%BA%AE%E7%9A%84%E4%BA%BA+%28+feat.%E9%BB%83%E7%8E%A0+%29+%28Can%E2%80%99t+sleep+%28+feat.Dadado+Huang+%29%29",
+      ],
+    },
+    {
+      id: 12,
+      year: "2021",
+      title1: "PUZZLEMAN——",
+      title2: "打打多多",
+      feat: "(feat. 黃玠)",
+      sImg: S12,
+      link: [
+        "https://music.apple.com/tw/album/%E6%89%93%E6%89%93%E5%A4%9A%E5%A4%9A-feat-%E9%BB%83%E7%8E%A0/1558991144?i=1558991162",
+        "https://open.spotify.com/track/0x8999C3GJtJk2UmEdV3SV?si=7a211628c4964ac5",
+        "https://kkbox.fm/Wsln03?utm_source=share&utm_medium=song&utm_campaign=PUZZLEMAN+-+%E6%89%93%E6%89%93%E5%A4%9A%E5%A4%9A+feat.+%E9%BB%83%E7%8E%A0",
+      ],
     },
   ];
   const [page, setPage] = useState(0);
@@ -201,41 +325,28 @@ const Music = () => {
           <div className={styles.inText2}>最佳精選特輯</div>
           <div className={styles.inText3}>黃玠×NABOWA</div>
           <div className={styles.mediaM}>
-            <div className={styles.imusicM}></div>
-            <div className={styles.spotifyM}></div>
-            <div className={styles.kkboxM}></div>
+            <a href={musicData.link[0]}>
+              <div className={styles.imusicM}></div>
+            </a>
+            <a href={musicData.link[1]}>
+              <div className={styles.spotifyM}></div>
+            </a>
+            <a href={musicData.link[2]}>
+              <div className={styles.kkboxM}></div>
+            </a>
           </div>
           <div className={styles.listM}>
-            <div>
-              <div className={styles.number}>1</div>| 相片蒐集簿
-            </div>
-            <div>
-              <div className={styles.number}>2</div>| 在一片黑暗
-            </div>
-            <div>
-              <div className={styles.number}>3</div>| 關於那些記得
-            </div>
-            <div>
-              <div className={styles.number}>4</div>| 你
-            </div>
-            <div>
-              <div className={styles.number}>5</div>| 相片蒐集簿
-            </div>
-            <div>
-              <div className={styles.number}>6</div>| 相片蒐集簿
-            </div>
-            <div>
-              <div className={styles.number}>7</div>| Party到天亮，相片蒐集簿
-            </div>
-            <div>
-              <div className={styles.number}>8</div>| 脆弱
-            </div>
-            <div>
-              <div className={styles.number}>9</div>| 我
-            </div>
-            <div>
-              <div className={styles.number}>10</div>| 相片蒐集簿
-            </div>
+            {musicData.song &&
+              musicData.song.map((song, i) => {
+                return (
+                  <div>
+                    <div className={styles.number}>
+                      {i + 1 < 10 ? `0${i + 1}` : i + 1}
+                    </div>
+                    | {song}
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
@@ -268,52 +379,33 @@ const Music = () => {
             </div>
             <div className={styles.media}>
               <div>
-                <a href="">
+                <a href={musicData.link[0]}>
                   <Image src={Imusic}></Image>
                 </a>
               </div>
               <div>
-                <a href="">
+                <a href={musicData.link[1]}>
                   <Image src={Spotify}></Image>
                 </a>
               </div>
               <div>
-                <a href="">
+                <a href={musicData.link[2]}>
                   <Image src={Kkbox}></Image>
                 </a>
               </div>
             </div>
             <div className={styles.list}>
-              <div>
-                <div className={styles.number}>1</div>| 相片蒐集簿
-              </div>
-              <div>
-                <div className={styles.number}>2</div>| 在一片黑暗
-              </div>
-              <div>
-                <div className={styles.number}>3</div>| 關於那些記得
-              </div>
-              <div>
-                <div className={styles.number}>4</div>| 你
-              </div>
-              <div>
-                <div className={styles.number}>5</div>| 相片蒐集簿
-              </div>
-              <div>
-                <div className={styles.number}>6</div>| 相片蒐集簿
-              </div>
-              <div>
-                <div className={styles.number}>7</div>| 相片蒐集簿
-              </div>
-              <div>
-                <div className={styles.number}>8</div>| 脆弱
-              </div>
-              <div>
-                <div className={styles.number}>9</div>| 我
-              </div>
-              <div>
-                <div className={styles.number}>10</div>| 相片蒐集簿
-              </div>
+              {musicData.song &&
+                musicData.song.map((song, i) => {
+                  return (
+                    <div>
+                      <div className={styles.number}>
+                        {i + 1 < 10 ? `0${i + 1}` : i + 1}
+                      </div>
+                      | {song}
+                    </div>
+                  );
+                })}
             </div>
             <div className={styles.sImgM} onClick={switchInnerPage}>
               <Image src={musics[musics.length - 1].sImg}></Image>
