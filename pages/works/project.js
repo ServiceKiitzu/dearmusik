f61907128d5b4940b3231865775dcf43c1e280ba
Frank Liao <frank_liao@kiitzu.com>
2022-06-20 13:33:16 +0800

    fix: project page layout( mobile )
    ref: http://fox.designforcode.cc/dearmusik/pm_dearmusik/-/issues/6#note_9454

--- pages/works/project.js
+++ pages/works/project.js
@@ -28,68 +28,70 @@ const Project = () => {
     <div className={styles.container}>
       <div className={styles.topic}>作品</div>
       <WorksNavM></WorksNavM>
-      <div className={styles.avatarBox}>
-        <Image src={Avatar}></Image>
-      </div>
-      <div
-        dangerouslySetInnerHTML={{ __html: t.text }}
-        className={styles.text}
-      ></div>
-      <WorksNav></WorksNav>
-      <div className={styles.imgBox}>
-        <div>
-          <Image src={Illustrator}></Image>
-          {t.illustrator}
+      <div className={styles.scrollBox}>
+        <div className={styles.avatarBox}>
+          <Image src={Avatar}></Image>
         </div>
-        <div>
-          <Image src={Youtuber}></Image>
-          {t.youtuber}
+        <div
+          dangerouslySetInnerHTML={{ __html: t.text }}
+          className={styles.text}
+        ></div>
+        <WorksNav></WorksNav>
+        <div className={styles.imgBox}>
+          <div>
+            <Image src={Illustrator}></Image>
+            {t.illustrator}
+          </div>
+          <div>
+            <Image src={Youtuber}></Image>
+            {t.youtuber}
+          </div>
+          <div>
+            <Image src={Cooker}></Image>
+            <div style={{ marginTop: "7px" }}>{t.cooker}</div>
+          </div>
+          <div>
+            <Image src={Pocaster}></Image>
+            {t.pocaster}
+          </div>
+          <div>
+            <Image src={Qone}></Image>
+            ______
+          </div>
+          <div>
+            <Image src={Qtwo}></Image>
+            ____
+          </div>
         </div>
-        <div>
-          <Image src={Cooker}></Image>
-          <div style={{ marginTop: "7px" }}>{t.cooker}</div>
+        <div className={styles.imgBoxM}>
+          <div>
+            <Image src={IllustratorM}></Image>
+            <span>{t.illustrator}</span>
+          </div>
+          <div>
+            <Image src={YoutuberM}></Image>
+            {t.youtuber}
+          </div>
+          <div>
+            <Image src={CookerM}></Image>
+            <span>{t.cooker}</span>
+          </div>
+          <div>
+            <Image src={PocasterM}></Image>
+            {t.pocaster}
+          </div>
+          <div>
+            <Image src={QoneM}></Image>
+            <span>______</span>
+          </div>
+          <div>
+            <Image src={QtwoM}></Image>
+            ____
+          </div>
         </div>
-        <div>
-          <Image src={Pocaster}></Image>
-          {t.pocaster}
+        <div className={styles.imgM}>
+          <Image src={ImgM}></Image>
         </div>
-        <div>
-          <Image src={Qone}></Image>
-          ______
-        </div>
-        <div>
-          <Image src={Qtwo}></Image>
-          ____
-        </div>
-      </div>
-      <div className={styles.imgBoxM}>
-        <div>
-          <Image src={IllustratorM}></Image>
-          {t.illustrator}
-        </div>
-        <div>
-          <Image src={YoutuberM}></Image>
-          {t.youtuber}
-        </div>
-        <div>
-          <Image src={CookerM}></Image>
-          {t.cooker}
-        </div>
-        <div>
-          <Image src={PocasterM}></Image>
-          {t.pocaster}
-        </div>
-        <div>
-          <Image src={QoneM}></Image>
-          ______
-        </div>
-        <div>
-          <Image src={QtwoM}></Image>
-          ____
-        </div>
-      </div>
-      <div className={styles.imgM}>
-        <Image src={ImgM}></Image>
       </div>
     </div>
   );
