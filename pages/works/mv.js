3aefb4768d2850d2f5f93c3c41454ece76fb1126
Frank Liao <frank_liao@kiitzu.com>
2022-06-23 01:48:25 +0800

    fix: mv play bug try

--- pages/works/mv.js
+++ pages/works/mv.js
@@ -198,15 +198,6 @@ const Mv = () => {
                       opts={{
                         width: "100%",
                         height: "100%",
-                        playerVars: {
-                          disablekb: 1,
-                          playlist: item.youtube,
-                          loop: 0,
-                          playsinline: 0,
-                          rel: 0,
-                          fs: 1,
-                          enablejsapi: 1,
-                        },
                       }}
                       onReady={(e) => storeMobileYoutube(e, index)}
                       onPlay={fullScreen}
