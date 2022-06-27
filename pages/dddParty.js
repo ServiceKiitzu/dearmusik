ae79dbc52b0cf486c4b345e374029bef148da476
Frank Liao <frank_liao@kiitzu.com>
2022-06-27 16:00:43 +0800

    fix: party page footer color

--- pages/dddParty.js
+++ pages/dddParty.js
@@ -2,8 +2,16 @@ import styles from "../styles/Party.module.scss";
 import Image from "next/image";
 import Soon from "../assets/ddd-party/soon.svg";
 import SoonM from "../assets/ddd-party/soon-m.svg";
+import { useEffect } from "react";
 
 const DDDParty = () => {
+  useEffect(() => {
+    const footer = document.getElementById("footer");
+    footer.style.backgroundColor = "transparent";
+    return function cleanUp() {
+      footer.style.backgroundColor = "#000";
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.img}>
