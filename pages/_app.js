2a8fc0ce6adfa30f85a4208e350cd1b20b29544a
Frank Liao <frank_liao@kiitzu.com>
2022-07-06 13:41:49 +0800

    fix: gtm problem fix

--- pages/_app.js
+++ pages/_app.js
@@ -12,30 +12,11 @@ function MyApp({ Component, pageProps }) {
   const [type, setType] = useState("全部");
   return (
     <>
-      {/* Google Tag Manager */}
-      <Script
-        id="google-gtm"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WH93B9G');`,
-        }}
-      />
       <TypeContext.Provider value={{ type, setType }}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </TypeContext.Provider>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WH93B9G"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-`,
-        }}
-      ></noscript>
     </>
   );
 }
