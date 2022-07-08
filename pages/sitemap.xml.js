ce15480626d10d20b60988e37c1f260a5df0a005
Frank Liao <frank_liao@kiitzu.com>
2022-07-08 23:24:12 +0800

    fix: sitemap config

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,23 +1,11 @@
 // pages/sitemap.xml.js
 import { getServerSideSitemap } from "next-sitemap";
-import { otherLanguages } from "config/i18nConfig";
 export const getServerSideProps = async (ctx) => {
   const response = await fetch("https://dadadoking.com");
   const paths = await response.json();
   const fields = paths.data.map(({ path }) => {
-    const alternateRefs = otherLanguages.map((language) => {
-      let href = "https://dadadoking.com" + language + path;
-      if (language === "en") {
-        href = "https://dadadoking.com" + path;
-      }
-      return {
-        href,
-        hreflang: [...otherLanguages],
-      };
-    });
     return {
       loc: "https://dadadoking.com" + path,
-      alternateRefs,
       lastmod: new Date().toISOString(),
     };
   });
